372a012c0d8c79ba2cd89559c688e0459198baaa
kateVoice <kate.voicekhovskaya@gmail.com>
2022-07-21 17:40:30 +0300

    git add file

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -1,7 +1,7 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello git!");
+        System.out.println("Hello Rebase!");
         System.out.println("Hello java!!");
     }
 }
