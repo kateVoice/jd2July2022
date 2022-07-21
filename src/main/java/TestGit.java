8ec710b33ea80b6c87c26b431bb5ea42b26ec0b5
kateVoice <kate.voicekhovskaya@gmail.com>
2022-07-21 17:40:30 +0300

    git test rebase2

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -1,7 +1,9 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
         System.out.println("Hello Rebase!");
+
         System.out.println("Hello java!!");
     }
 }
